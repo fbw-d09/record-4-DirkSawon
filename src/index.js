84cd699832c3bed6f25005b7b2e46364680d71eb
DirkSawon <Dirk.Sawon@gmx.de>
2023-08-28 14:14:27 +0200

    Version 0.03 - In progress

--- src/index.js
+++ src/index.js
@@ -15,8 +15,38 @@ app.use(bodyParser.json());
 
 const port = process.env.PORT;
 
-app.post('/Orders/', async (req, res) => {
+app.post('/orders/', async (req, res) => {
     console.log(req.body);
 
-    const { }
+    const { id, qty } = req.body;
+
+    try {
+        connect().then(async (db) => {
+            const newOrder = new Order({
+                id,
+                qty
+            });
+
+            console.log(newOrder);
+
+            newOrder
+            .save()
+            .then(doc => {
+                res.status(201).json({
+                    success: true,
+                    data: doc
+                })
+            })
+            .catch(err => {
+                res.status(400).json({
+                    success: false,
+                    message: err.message
+                });
+            })
+
+        })
+        
+    } catch (error) {
+        console.log(error.message);
+    }
 })
