56c5ecbe890701e1285ab85a74e09c45797617ce
DirkSawon <Dirk.Sawon@gmx.de>
2023-08-29 15:25:48 +0200

    Version 0.16 - Seeding for orders and records added

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const users = require('./routes/users.js');
 const orders = require('./routes/orders.js');
 const records = require('./routes/records.js');
 
+const Order = require('./models/Order.js');
+const Record = require('./models/Record.js');
 const User = require('./models/User.js');
 
 const { connect, closeConnection } = require('./configs/db.js');
@@ -28,13 +30,7 @@ if (args[0] === "init" && args[1] === "-db") {
     const chanceLastname = chance.last();
     const chanceCompany = chance.company().replace(' ', '');
     const chanceEmail = `${ chanceFirstname }.${ chanceLastname }@provider.${ chance.country().toLowerCase() }`;
-    const chancePassword = chance.string({ length: 10 });
-
-    console.log("random firstname:", chanceFirstname);
-    console.log("random lastname:", chanceLastname);
-    console.log("random company:", chanceCompany);
-    console.log("random email:", chanceEmail);
-    console.log("random pw:", chancePassword); */
+    const chancePassword = chance.string({ length: 10 });*/
 
     // Script from Faker tutorial:
 
@@ -58,15 +54,6 @@ if (args[0] === "init" && args[1] === "-db") {
           const lastname = chance.last();
           const email = chance.email();
           const password = chance.string({ length: 10 });
-
-          /* const fullName = faker.name.firstName();
-          const ratings = faker.datatype.number({ min: 1, max: 5 });
-          const reviews = faker.lorem.sentences(3);
-          const location = faker.lorem.sentences(1);
-          const password = faker.datatype.number();
-          const email = faker.internet.email();
-          const category = faker.commerce.department();
-          const createdAt = faker.date.past(); */
       
           user.push({
             id,
@@ -88,6 +75,65 @@ if (args[0] === "init" && args[1] === "-db") {
             console.error(err);
             console.error(`${err.writeErrors?.length ?? 0} errors occurred during the insertMany operation.`);
   });
+
+  const generateRecords = (num) => {
+    const record = [];
+  
+    for (let i = 0; i < num; i++) {
+      const id = i+1;
+      const title = chance.sentence({ words: 5 });
+      const artist = chance.sentence({ words: 2 });
+      const year = chance.integer({ min: 1950, max: 2024});
+      const cover = `https://www.record-shop.com/${chance.string(length: 5)}`;
+      const price = chance.integer({ min: 10, max: 20 });
+  
+      record.push({
+        id,
+        title,
+        artist,
+        year,
+        cover,
+        price
+      });
+    }
+  
+    return record;
+  };
+
+  const record = generateRecords(50);
+
+  Record.insertMany(record)
+    .then(docs => console.log(`${docs.length} records have been inserted into the database.`))
+    .catch(err => {
+        console.error(err);
+        console.error(`${err.writeErrors?.length ?? 0} errors occurred during the insertMany operation.`);
+    });
+
+    const generateOrders = (num) => {
+        const order = [];
+      
+        for (let i = 0; i < num; i++) {
+          const id = i+1;
+          const qty = chance.integer({ min: 1, max: 20});
+      
+          order.push({
+            id,
+            qty,
+          });
+        }
+      
+        return order;
+      };
+    
+      const order = generateOrders(50);
+    
+      Order.insertMany(order)
+        .then(docs => console.log(`${docs.length} orders have been inserted into the database.`))
+        .catch(err => {
+            console.error(err);
+            console.error(`${err.writeErrors?.length ?? 0} errors occurred during the insertMany operation.`);
+        });
+
 };
 
 app.use(bodyParser.urlencoded({ extended: true }));
