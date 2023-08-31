c419270835e2b38f9971d8152edc046545f5a7bb
DirkSawon <Dirk.Sawon@gmx.de>
2023-08-31 14:51:54 +0200

    Version 0.20 - In progress

--- src/controller/users.js
+++ src/controller/users.js
@@ -66,9 +66,55 @@ exports.getAllUsers = async (req, res) => {
 
 // Brain-Stormen wie eine Filterfunktion via GET Anfrage realisiert werden kann (z.B. über Routen, welche Parameter übergeben werden, usw.)
 exports.filterUser = async (req, res) => {
-    console.log(req.body);
+    //console.log(req.body);
 
-    const { key, filter, value } = req.body;
+    const { criteria, filter1, filter2, filter3 } = req.body;
+
+    let f1 = false;
+    let f2 = false;
+    let f3 = false;
+
+    let key1 = "";
+    let key2 = "";
+    let key3 = "";
+
+    let rel1 = "";
+    let rel2 = "";
+    let rel3 = "";
+
+    let val1 = ""
+    let val2 = "";
+    let val3 = "";
+
+    if(filter1 !== undefined) {
+        const filter1WordArray = filter1.split(' ');
+        key1 = filter1WordArray[0];
+        rel1 = filter1WordArray[1];
+        val1 = filter1WordArray[2];
+        f1 = true;
+    }
+    
+    if(filter2 !== undefined) {
+        const filter2WordArray = filter2.split(' ');
+        key2 = filter2WordArray[0];
+        rel2 = filter2WordArray[1];
+        val2 = filter2WordArray[2];
+        f2 = true
+    }
+    
+    if(filter3 !== undefined) {
+        const filter3WordArray = filter3.split(' ');
+        key3 = filter3WordArray[0];
+        rel3 = filter3WordArray[1];
+        val3 = filter3WordArray[2];
+        f3 = true;
+    }
+    
+
+
+    //console.log(key1, rel1, val1);
+    //console.log(key2, rel2, val2);
+    //console.log(key3, rel3, val3);
 
     // Filternamen/Methoden definieren:
     /**  
@@ -76,11 +122,93 @@ exports.filterUser = async (req, res) => {
     /*  Ein Suchkriterium: .where(Schlüssel)."filter(Wert)"
     /*  Mehrere Kritierien: .where(Schlüssel_1)."filter_1(Wert_1)".where(Schlüssel_2)."filter_2(Wert2).[...].where(Schlüssel_n)."filter_n(Wert_n)"
     /* z.B. schlüssel, filter, wert
+    /* Alle Variationen abdecken: 3 * 2^n Fälle
     **/
+    
+    // if()
+    switch ( criteria ) {
+        case '1':
+            try {
+                connect().then(async (db) => {
+                    User
+                    .where(key1)
+                    .equals(val1)
+                    .then(docs => {
+                        res.status(200).json({
+                            success: true,
+                            data: docs
+                        })
+                    })
+                    .catch(err => {
+                        res.status(404).json({
+                            success: false,
+                            message: err.message
+                        })
+                    })
+                })
+            } catch (error) {
+                console.log(error.message);
+            }
+        break;
 
-    console.log(filter);
+        case '2':
+            try {
+                connect().then(async (db) => {
+                    User
+                    .where(key1)
+                    .equals(val1)
+                    .where(key2)
+                    .equals(val2)
+                    .then(docs => {
+                        res.status(200).json({
+                            success: true,
+                            data: docs
+                        })
+                    })
+                    .catch(err => {
+                        res.status(404).json({
+                            success: false,
+                            message: err.message
+                        })
+                    })
+                })
+            } catch (error) {
+                console.log(error.message);
+            }
+        break;
 
-    res.status(200).json({ success: true, key: key, filter: filter, value: value });
+        case '3':
+            try {
+                connect().then(async (db) => {
+                    User
+                    .where(key1)
+                    .equals(val1)
+                    .where(key2)
+                    .equals(val2)
+                    .where(key3)
+                    .equals(val3)
+                    .then(docs => {
+                        res.status(200).json({
+                            success: true,
+                            data: docs
+                        })
+                    })
+                    .catch(err => {
+                        res.status(404).json({
+                            success: false,
+                            message: err.message
+                        })
+                    })
+                })
+            } catch (error) {
+                console.log(error.message);
+            }
+        break;
+    }
+
+    //console.log(filters);
+
+    //res.status(200).json({ success: true, criteria: criteria, filter1: filter1, filter2: filter2, filter3: filter3 });
 };
 
 exports.getUser = async (req, res) => {
